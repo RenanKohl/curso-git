78279899f82c68f9d659713ad957d597489247b5
Renan Kohl <renan.kohl@hotmail.com>
2018-06-01 23:42:37 -0300

    Acrescentando uma mensagem de despedida

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -14,8 +14,10 @@ namespace projeto_teste
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
 
-            Console.ReadLine();
+
+           Console.ReadLine();
         }
     }
 }
